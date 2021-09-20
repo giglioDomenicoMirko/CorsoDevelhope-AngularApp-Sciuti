59ccb839dbfa35e7424c3bf5111927b3d38e482b
giglioDomenicoMirko <mirko.giglio@gmail.com>
2021-09-20 20:57:09 +0200

    Creiamo componenti per altri progetti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProjectComponent } from './project/project/project.component';
+import { ProjectDetailComponent } from './project/project-detail/project-detail.component';
+import { ProjectListComponent } from './project/project-list/project-list.component';
+import { ProjectFormComponent } from './project/project-form/project-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectComponent
+    ProjectComponent,
+    ProjectDetailComponent,
+    ProjectListComponent,
+    ProjectFormComponent
   ],
   imports: [
     BrowserModule,
