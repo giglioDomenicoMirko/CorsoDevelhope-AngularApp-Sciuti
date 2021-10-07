21ed65a2bfcb7e762c6a8d5b5f7ef5c23355b48b
giglioDomenicoMirko <mirko.giglio@gmail.com>
2021-10-07 23:35:32 +0200

    Costruiamo il servizio per i progetti\

--- src/app/project/project-container/project-container.component.ts
+++ src/app/project/project-container/project-container.component.ts
@@ -12,40 +12,7 @@ export class ProjectComponent implements OnInit {
 
   selectedProject!: Project;
 
-  projects: Project[] = [
-    {
-      id: 1,
-      code: 'NHusYJl',
-      name: 'Progetto Alpha',
-      description: 'Lorem ipsum dolor sit amet.',
-      start: new Date(2021, 1, 30),
-      end: new Date(2021, 3, 15),
-      priority: 'medium',
-      done: true,
-      tasks: []
-    },
-    {
-      id: 2,
-      code: 'SJieYKL',
-      name: 'Progetto Beta',
-      description: 'Lorem ipsum dolor sit amet.',
-      start: new Date(2019, 3, 30),
-      end: new Date(2019, 6, 15),
-      priority: 'low',
-      done: true,
-      tasks: []
-    },
-    {
-      id: 3,
-      code: 'POjeGBs',
-      name: 'Progetto Gamma',
-      description: 'Lorem ipsum dolor sit amet.',
-      start: new Date(2019, 8, 15),
-      priority: 'low',
-      done: false,
-      tasks: []
-    }
-  ]
+  projects: Project[] = [];
 
   constructor(private logService: LogService) {
     this.logService.log('Costruttore del Prj Cmp');
---
+++ src/app/project/project.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Project } from '@app/models/Project';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+
+  projects: Project[] = [
+    {
+      id: 1,
+      code: 'NHusYJl',
+      name: 'Progetto Alpha',
+      description: 'Lorem ipsum dolor sit amet.',
+      start: new Date(2021, 1, 30),
+      end: new Date(2021, 3, 15),
+      priority: 'medium',
+      done: true,
+      tasks: []
+    },
+    {
+      id: 2,
+      code: 'SJieYKL',
+      name: 'Progetto Beta',
+      description: 'Lorem ipsum dolor sit amet.',
+      start: new Date(2019, 3, 30),
+      end: new Date(2019, 6, 15),
+      priority: 'low',
+      done: true,
+      tasks: []
+    },
+    {
+      id: 3,
+      code: 'POjeGBs',
+      name: 'Progetto Gamma',
+      description: 'Lorem ipsum dolor sit amet.',
+      start: new Date(2019, 8, 15),
+      priority: 'low',
+      done: false,
+      tasks: []
+    }
+  ]
+
+  constructor() { }
+
+  getAll(): Project[] {
+    return this.projects;
+  }
+
+  add(project: Project): void {
+    this.projects.push(project);
+  }
+
+  get(id: number): Project {
+    return this.projects.find(project => project.id === id) as Project;
+  }
+
+}
