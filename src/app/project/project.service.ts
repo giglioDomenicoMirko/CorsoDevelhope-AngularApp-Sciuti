1f010b1e5d1a116f306c2e8b7c971785f56db160
giglioDomenicoMirko <mirko.giglio@gmail.com>
2021-10-14 23:17:00 +0200

    CatchError e Retry su HttpClient

--- src/app/project/project.service.ts
+++ src/app/project/project.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Project } from '@app/models/Project';
 import { LogService } from '@app/shared/log.service';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, retry, tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -52,7 +52,9 @@ export class ProjectService {
   getAll(): Observable<Project[]> {
     return this.httpClient.get<Project[]>('http://localhost:3000/projects')
     .pipe(
-      tap((data) => this.logService.log(`getAll Eseguito ${data}`))
+      tap((data) => this.logService.log(`getAll Eseguito ${data}`)),
+      retry(3),
+      catchError(this.handleError)
     );
     // this.logService.log('getAll Eseguito');
     // return this.project$.pipe(
@@ -71,7 +73,11 @@ export class ProjectService {
     }
     return this.httpClient.post<Project>('http://localhost:3000/projects', projectToAdd)
     .pipe(
-      tap((data) => this.logService.log(`add Eseguito ${data}`))
+      tap((data) => this.logService.log(`add Eseguito ${data}`)),
+      retry(3),
+      catchError(this.handleError)
+
+
     );
     // this.logService.log('add Eseguito');
     // this.projects.push({
@@ -87,10 +93,25 @@ export class ProjectService {
   get(id: number): Observable<Project> {
     return this.httpClient.get<Project>(`http://localhost:3000/projects/${id}`)
     .pipe(
-      tap((data) => this.logService.log(`get Eseguito ${data}`))
+      tap((data) => this.logService.log(`get Eseguito ${data}`)),
+      retry(3),
+      catchError(this.handleError)
+
+
     );
     // this.logService.log('get Eseguito');
     // return this.projects.find(project => project.id === id) as Project;
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, `+
+        `body was: ${error.error}`);
+    }
+    return throwError('Something bad happened; please try again later.');
+  };
 
 }
