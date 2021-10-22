8650f822e2d20f60527bdcb630ef60754477c608
giglioDomenicoMirko <mirko.giglio@gmail.com>
2021-10-22 17:00:32 +0200

    Preleviamo il parametro dallo snapshot con ActivatedRoute

--- src/app/project/project-detail/project-detail.component.ts
+++ src/app/project/project-detail/project-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Project } from '@app/models/Project';
+import { ProjectService } from '../project.service';
 
 @Component({
   selector: 'ngprj-project-detail',
@@ -10,10 +12,15 @@ export class ProjectDetailComponent implements OnInit {
 
   @Input() project!: Project;
 
-  constructor() {
-  }
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private projectService: ProjectService
+    ) {  }
 
   ngOnInit(): void {
+    // console.log(this.activateRoute.snapshot.paramMap.get('id'));
+    const id = this.activateRoute.snapshot.paramMap.get('id');
+    this.projectService.get(+id!).subscribe(data => console.log(data))
   }
 
 }
