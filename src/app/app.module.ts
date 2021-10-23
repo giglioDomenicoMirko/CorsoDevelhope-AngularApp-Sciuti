3fa6b704bd77a3bf7faf531b1d813573e48e63f7
giglioDomenicoMirko <mirko.giglio@gmail.com>
2021-10-23 18:32:45 +0200

    Project Module - Spostiamo i componenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,36 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProjectDashboardComponent } from './modules/project/pages/project-dashboard/project-dashboard.component';
-import { ProjectDetailComponent } from './modules/project/pages/project-detail/project-detail.component';
-import { ProjectListComponent } from './modules/project/component/project-list/project-list.component';
-import { ProjectFormComponent } from './modules/project/component/project-form/project-form.component';
+// import { ProjectDashboardComponent } from './modules/project/pages/project-dashboard/project-dashboard.component';
+// import { ProjectDetailComponent } from './modules/project/pages/project-detail/project-detail.component';
+// import { ProjectListComponent } from './modules/project/component/project-list/project-list.component';
+// import { ProjectFormComponent } from './modules/project/component/project-form/project-form.component';
 import { HomeComponent } from './modules/dashboard/pages/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http'
-import { Sharedmodule } from './shared/shared.module';
+// import { Sharedmodule } from './shared/shared.module';
+import { ProjectModule } from './modules/project/project.modules';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectDashboardComponent,
-    ProjectDetailComponent,
-    ProjectListComponent,
-    ProjectFormComponent,
+    // ProjectDashboardComponent,
+    // ProjectDetailComponent,
+    // ProjectListComponent,
+    // ProjectFormComponent,
     HomeComponent,
     NavbarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    // FormsModule,
     AppRoutingModule,
-    Sharedmodule,
-    HttpClientModule
+    // Sharedmodule,
+    HttpClientModule,
+    ProjectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/project/project.modules.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Sharedmodule } from "@app/shared/shared.module";
+import { ProjectFormComponent } from "./component/project-form/project-form.component";
+import { ProjectListComponent } from "./component/project-list/project-list.component";
+import { ProjectDashboardComponent } from "./pages/project-dashboard/project-dashboard.component";
+import { ProjectDetailComponent } from "./pages/project-detail/project-detail.component";
+
+@NgModule ({
+    declarations: [
+        ProjectDashboardComponent,
+        ProjectDetailComponent,
+        ProjectListComponent,
+        ProjectFormComponent
+    ],
+    imports: [
+        Sharedmodule,
+        CommonModule,
+        FormsModule
+    ]
+})
+export class ProjectModule {}
